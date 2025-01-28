42b234590e0c7c31b8e65da18d4c073b9cf74435
CometB <126880018+CometB@users.noreply.github.com>
2025-01-28 07:50:48 +0700

    .

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,32 @@ int main(){
 }
 
 //Do not modify source code above this line 
+void inputMatrix(double A[N][N]){
+    for(int i=0;i<N;i++){
+        cout << "Row " << i+1 << ": ";
+        for(int j=0;j<N;j++){
+            cin >> A[i][j];
+        }
+    }
+}
+
+void findLocalMax(const double A[N][N], bool B[N][N]){
+    for(int i=1;i<N-1;i++){
+        for(int j=1;j<N-1;j++){
+            if((A[i][j]>=A[i][j-1]) && (A[i][j]>=A[i-1][j]) && (A[i][j]>=A[i][j+1]) && (A[i][j]>=A[i+1][j])){
+                B[i][j] = 1;
+            } else{
+                B[i][j] = 0;
+            }
+        }
+    }
+}
+
+void showMatrix(const bool A[N][N]){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout << A[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
